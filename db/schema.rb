be3857eb2b7393579ad2feb422a05980f6329e76
Sooyung Kim <kimx1279@gmail.com>
2024-01-24 12:03:45 -0700

    Update relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_002338) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_24_190130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_002338) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "subscription_teas", force: :cascade do |t|
+    t.bigint "subscription_id", null: false
+    t.bigint "tea_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subscription_id"], name: "index_subscription_teas_on_subscription_id"
+    t.index ["tea_id"], name: "index_subscription_teas_on_tea_id"
   end
 
   create_table "subscriptions", force: :cascade do |t|
@@ -41,10 +50,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_002338) do
     t.string "brew_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "subscription_id", null: false
-    t.index ["subscription_id"], name: "index_teas_on_subscription_id"
   end
 
+  add_foreign_key "subscription_teas", "subscriptions"
+  add_foreign_key "subscription_teas", "teas"
   add_foreign_key "subscriptions", "customers"
-  add_foreign_key "teas", "subscriptions"
 end
