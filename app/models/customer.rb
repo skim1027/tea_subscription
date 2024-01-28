3d5cbf9b3822f2f294f16bbb5e2ad86bb31e7e6d
Sooyung Kim <kimx1279@gmail.com>
2024-01-27 20:22:15 -0700

    Add model test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
   has_many :subscriptions
+  has_many :teas, through: :subscriptions
   
   validates :first_name, presence: true
   validates :last_name, presence: true
